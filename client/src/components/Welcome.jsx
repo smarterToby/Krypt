26962bb5a5525a1700be17a56ae6ac1aefdeb24a
smarterToby <84527857+smartertoby@users.noreply.github.com>
2024-05-04 11:20:24 +0200

    fix: react-icons import

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,7 @@
 import React, {useContext} from "react";
-import {AiFillPlayCircle} from "react-icons/all.js";
-import {SiEthereum} from "react-icons/all.js";
-import {BsInfoCircle} from "react-icons/all.js";
+import {AiFillPlayCircle} from "react-icons/ai";
+import {SiEthereum} from "react-icons/si";
+import {BsInfoCircle} from "react-icons/bs";
 
 import {Loader} from "./";
 
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-    solidity: "0.8.0",
-    networks: {
-        goerli: {
-            url: "https://eth-goerli.g.alchemy.com/v2/QIKOGGLiQJ53N6gqIjayWtmvFkxfRGOC",
-            accounts: [
-                '99adb67bb47f15fb1ce4e607bf57b148dee39a524bf6891dd6b98524b2510527'
-            ]
-        }
-    }
-}
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.GOERLI_ACCCOUNT],
+    },
+  },
+};
